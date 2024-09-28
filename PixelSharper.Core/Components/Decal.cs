a279d03a9402e669691fac06e07a7c1929c9dbfe
propagating <mail@ryanrichardson.org>
2024-09-27 17:43:16 -0700

    Scaffolded Decal class and added appropriate enums

---
+++ PixelSharper.Core/Components/Decal.cs
@@ -0,0 +1,42 @@
+﻿using PixelSharper.Core.Types;
+
+namespace PixelSharper.Core.Components;
+
+public class Decal
+{
+    public int Id { get; set; }
+    public Sprite Sprite { get; set; }
+    public Vector2d<float> UVScale { get; set; }
+    
+    public Decal()
+    {
+        Id = -1;
+        Sprite = null;
+        UVScale = new Vector2d<float>(1.0f, 1.0f);
+    }
+    
+    public Decal(Sprite sprite, bool filter = false, bool clamp = true)
+    {
+        
+    }
+
+    public Decal(uint existingTextureResource, Sprite sprite)
+    {
+        
+    }
+
+    public void Update()
+    {
+        
+    }
+
+    public void UpdateSprite()
+    {
+        
+    }
+
+    ~Decal()
+    {
+        
+    }
+}
---
+++ PixelSharper.Core/Enums/DecalMode.cs
@@ -0,0 +1,11 @@
+﻿namespace PixelSharper.Core.Enums;
+
+public enum DecalMode : byte
+{
+    Normal         = 0x01,
+    Additive       = 0x02,
+    Multiplicative = 0x04,
+    Stencil        = 0x08,
+    Illuminate     = 0x10,
+    Wireframe      = 0x20
+}
